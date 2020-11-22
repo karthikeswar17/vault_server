04dd68b4edc3769d90d7f07decedfbd0c0d41bda
karthikeswar17 <karthikeswar173@gmail.com>
2020-11-22 19:15:52 +0530

    added login api

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from typing import Optional, List
 
-from fastapi import FastAPI,Depends,HTTPException,status
+from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 from sqlalchemy.orm import Session
-import crud, models, schemas
+import crud
+import models
+import schemas
 from database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
@@ -13,13 +15,16 @@ app = FastAPI()
 
 security = HTTPBasic()
 
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-def get_current_username(credentials: HTTPBasicCredentials = Depends(security),db: Session = Depends(get_db)):
+
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security), db: Session = Depends(get_db)):
     user = crud.get_user(db, credentials.username)
     hash_pass = credentials.password
     # raise Exception(user.password,hash_pass)
@@ -30,27 +35,46 @@ def get_current_username(credentials: HTTPBasicCredentials = Depends(security),d
             headers={"WWW-Authenticate": "Basic"},
         )
     return credentials.username
-    
+
+
 @app.post("/accounts", response_model=schemas.Account)
-def create_account(account:schemas.AccountCreate ,username: str = Depends(get_current_username),db: Session = Depends(get_db)):
+def create_account(account: schemas.AccountCreate, username: str = Depends(get_current_username), db: Session = Depends(get_db)):
     user_id = crud.get_user(db, username).id
     users = crud.create_account(db, account, user_id)
     return users
 
+
 @app.post("/user", response_model=schemas.User)
-def create_user(user:schemas.UserCreate ,db: Session = Depends(get_db)):
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     users = crud.create_user(db, user)
     return users
 
+
 @app.get("/accounts", response_model=List[schemas.Account])
-def get_all_accounts(username: str = Depends(get_current_username),db: Session = Depends(get_db)):
+def get_all_accounts(username: str = Depends(get_current_username), db: Session = Depends(get_db)):
     user = crud.get_user(db, username)
     if user:
         accounts = user.accounts
         return accounts
-    
+
+
+@app.get("/login",)
+def get_all_users(credentials: HTTPBasicCredentials = Depends(security), db: Session = Depends(get_db)):
+    user = crud.get_user(db, credentials.username)
+    hash_pass = credentials.password
+    # raise Exception(user.password,hash_pass)
+    if not user or hash_pass != user.password:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail={"status": "fail",
+                    "message": "Incorrect username or password"},
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    else:
+        return {"status": "success"}
+
 
 @app.get("/user", response_model=List[schemas.User],)
-def get_all_users(username: str = Depends(get_current_username),db: Session = Depends(get_db)):
+def get_all_users(username: str = Depends(get_current_username), db: Session = Depends(get_db)):
     users = crud.get_users(db)
     return users
